359cdd2d53e7a2389922357f9eb07836b56e95a4
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-31 21:47:48 +0200

    Skip funzionante

--- main.js
+++ main.js
@@ -2,29 +2,32 @@ var app = new Vue({
     el: '#root',
     data: {
         image: [
-            'https://source.unsplash.com/random/1520x880/?landscape',
-            'https://source.unsplash.com/random/1520x880/?landscape,city',
-            'https://source.unsplash.com/random/1520x880/?landscape,japann',
-            'https://source.unsplash.com/random/1520x880/?landscape,rome'
+            'https://source.unsplash.com/random/200x880/?landscape',
+            'https://source.unsplash.com/random/200x880/?landscape,city',
+            'https://source.unsplash.com/random/200x880/?landscape,japann',
+            'https://source.unsplash.com/random/200x880/?landscape,rome'
         ],
         setIndex: 0,
         skip: null,
     },
     methods: {
-        autoSkip: function() {
-            this.skip = setInterval(checkImage, 1000)
-        },
-
-        checkImage: function(){
-            if(setIndex == this.image.lenght-1){
-                setIndex = 0; 
+        nextImage: function(){
+            if(this.setIndex == this.image.length-1){
+                this.setIndex = 0
             } else {
-                setIndex++
+                this.setIndex++
             }
-            console.log(Skip);
+        },
+        previousImage: function(){
+            if(this.setIndex == 0){
+                this.setIndex = this.image.length-1
+            } else{
+                this.setIndex--
+            }
         }
-
-        
+    },
+    mounted(){
+        this.skip = setInterval(this.nextImage, 3000)        
     }
 }
 )
